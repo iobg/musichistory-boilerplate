9cce524b6716b4b8e760e4eac0a4b936d84f83b4
Banks Humphreys <iobg@Bankss-MacBook-Pro.local>
2016-07-13 11:43:38 -0500

    cleaned up some js

--- dist/bundle.js
+++ dist/bundle.js
@@ -110,36 +110,36 @@ var filtering=require("./filtering");
 		loadSongs();
 	}
 	function loadSongs(){
-	var artistSelect=$("#Artists");
-	var albumSelect=$("#Albums");
-	var songList =$('#right');
-	var artists="<option selected='selected' disabled='disabled'>Artists</option>";
-	var albums="<option selected='selected' disabled='disabled'>Albums</option>";
-	songList.html("");
-	var songDOM="";
-	songs.forEach(function(song){
-	songDOM += "<div class='song'>";
-	songDOM += "<div class='title'>"+ song.title +"</div>";
-	songDOM += "<div class='artist'>"+ song.artist + "</div>";
-	songDOM += "<div class='album'>"+song.album +"</div>";
-	songDOM += "<input type='button' class='deletebtn' value='delete'>";
-	songDOM += "</div>";
-	artists+=`<option value="${song.artist}">${song.artist}</option>`;
-	albums+=`<option value="${song.album}">${song.album}</option>`;
+		var artistSelect=$("#Artists");
+		var albumSelect=$("#Albums");
+		var songList =$('#right');
+		var artists="<option selected='selected' disabled='disabled'>Artists</option>";
+		var albums="<option selected='selected' disabled='disabled'>Albums</option>";
+		songList.html("");
+		var songDOM="";
+		songs.forEach(function(song){
+			songDOM += "<div class='song'>";
+			songDOM += "<div class='title'>"+ song.title +"</div>";
+			songDOM += "<div class='artist'>"+ song.artist + "</div>";
+			songDOM += "<div class='album'>"+song.album +"</div>";
+			songDOM += "<input type='button' class='deletebtn' value='delete'>";
+			songDOM += "</div>";
+			artists+=`<option value="${song.artist}">${song.artist}</option>`;
+			albums+=`<option value="${song.album}">${song.album}</option>`;
 	});
-	artistSelect.html(artists);
-	albumSelect.html(albums);
-	songDOM += `<input type='button' id='more' value='more songs!'${disabled}>`;
-	songList.html(songDOM);
+		artistSelect.html(artists);
+		albumSelect.html(albums);
+		songDOM += `<input type='button' id='more' value='more songs!'${disabled}>`;
+		songList.html(songDOM);
 
-	$(".deletebtn").click(function(){
-		$(this).parent().remove();
+		$(".deletebtn").click(function(){
+			$(this).parent().remove();
 
-		});
+			});
 
-	var moreButton=$("#more");
-	moreButton.click(moreSongs);
-	filtering();
+		var moreButton=$("#more");
+		moreButton.click(moreSongs);
+		filtering();
 
 	}
 
